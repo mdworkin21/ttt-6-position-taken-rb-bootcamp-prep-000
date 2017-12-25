d5f501b8d9e5083a5034dff6da846ffbb2bdd015
Matthew Dworkin <mdworkin21@gmail.com>
2017-12-25 15:09:51 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,10 @@
 # code your #position_taken? method here!
 def position_taken?(array, index)
 
-  if array[index] == " " || "" || nil
+  if array[index] == " " 
     return false
+
+  elsif array[index] ==  "" || nil
 
   elsif array[index] == "X" || "O"
     return true
